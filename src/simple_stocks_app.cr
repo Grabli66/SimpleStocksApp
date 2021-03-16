544322beb55ecb8f250151cc1ce0e8722582e5bf
Grabli66 <grabli66@gmail.com>
2021-03-16 15:41:49 +0300

    Добавлена возможность получения значения доллара

--- src/simple_stocks_app.cr
+++ src/simple_stocks_app.cr
@@ -1,17 +1,37 @@
 require "kemal"
+require "./common"
 require "./moex_api"
 
 module SimpleStocksApp
   VERSION = "0.1.0"
   
-  get "/moex_last_hour/" do |env|
+  # Возвращает значение доллара
+  get "/dollar" do |env|
+    url = "https://www.cbr-xml-daily.ru/latest.js"
+      p url
+      response = Crest.get(
+        url
+      )
+
+      json = JSON.parse(response.body)
+      usd = 1.0 / json["rates"]["USD"].to_s.to_f
+
+      next {
+        "USD": usd
+      }.to_json
+  end
+
+  # Возвращает значение закрытия последней часовой свечи
+  get "/moex_last_hour" do |env|
     env.response.content_type = "application/json"
-    ticker = env.params.query["ticker"]
+    ticker = env.params.query["ticker"]    
 
     end_date = Time.local
     start_date = end_date - 7.days
-
+        
     candles = MoexApi.instance.get_chart_for_interval(ticker, MoexMarketType::Shares, IntervalType::Hour, start_date, end_date)
+    candles.sort { |a, b| a.date <=> b.date }    
+    
     last_candle = candles.last
 
     {
