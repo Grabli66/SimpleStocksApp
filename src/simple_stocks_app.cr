00c8650fae1739b0987e0858f4367f8c23077e64
Grabli66 <grabli66@gmail.com>
2021-06-01 22:01:28 +0300

    Сделана возможность получения данных с отступом

--- src/simple_stocks_app.cr
+++ src/simple_stocks_app.cr
@@ -30,23 +30,28 @@ module SimpleStocksApp
     env.response.content_type = "application/json"
     ticker = env.params.query["ticker"]?
     intervalStr = env.params.query["interval"]?
+    offsetStr = env.params.query["offset"]?
     if (ticker == nil) || (intervalStr == nil)
       halt env, status_code: 400, response: "Bad request"
     end
 
     interval = IntervalType.from_s(intervalStr.not_nil!)
+    offset = offsetStr.try &.to_i? || 0
 
+    # Начальные параметры запроса
     end_date = Time.local
-    start_date = end_date - 7.days
-
-    if interval == IntervalType::Month
-      start_date = end_date - 180.days
-    end
-
+    start_date = end_date
     requestInterval = interval
+
+    # Определяет параметры запроса
     case interval
     when IntervalType::Now
+      start_date = end_date - 7.days
       requestInterval = IntervalType::Day
+    when IntervalType::Day
+      start_date = end_date - (offset + 7).days
+    when IntervalType::Month
+      start_date = end_date - (offset + 3).months
     end
 
     candles = MoexApi.instance.get_chart_for_interval(ticker.not_nil!, MoexMarketType::Shares, requestInterval, start_date, end_date)
@@ -61,9 +66,15 @@ module SimpleStocksApp
       if candles.size > 0
         last_candle = candles[candles.size - 1]
       end
-    when IntervalType::Day, IntervalType::Month
+    when IntervalType::Day
       if candles.size > 1
-        last_candle = candles[candles.size - 2]
+        date = end_date - offset.days
+        last_candle = candles.find { |x| x.date >= date } || candles[candles.size - offset - 2]        
+      end
+    when IntervalType::Month
+      if candles.size > 1
+        date = end_date - offset.months
+        last_candle = candles.find { |x| x.date >= date } || candles[candles.size - offset - 2]        
       end
     end
 
